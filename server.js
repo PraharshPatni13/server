4241fdfc019f0be172f911a879eb8b68f83c64e0
PraharshPatni13 <praharshpatni@gmail.com>
2025-03-26 16:50:29 +0530

    add io in middleware

--- server.js
+++ server.js
@@ -22,7 +22,13 @@ app.use(express.json({ limit: '200mb' }));
 app.use(express.urlencoded({ extended: false, limit: '200mb' }))
 
 app.use(express.json());
-app.use(cors({}));
+app.use(cors());
+
+app.use((req, res, next) => {
+  req.io = io;
+  next();
+});
+
 
 
 const { send_welcome_page, send_otp_page } = require('./modules/send_server_email');
